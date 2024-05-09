e3683045a4b49f90888e3b5ca85f722e40446981
Krooby_ <jkocman07@gmail.com>
2024-05-09 18:54:18 +0200

    Server deployment

--- javascript/api.js
+++ javascript/api.js
@@ -1,4 +1,4 @@
-fetch('http://localhost:3000/leaderboard')
+fetch('https://osu-api.onrender.com/leaderboard')
     .then(response => response.json())
     .then(data => {
         console.log(data);
