cddb300b7ee6faad94256ca0aa14fae1a3b6d2aa
Krooby_ <jkocman07@gmail.com>
2024-05-10 17:13:02 +0200

    flag system

--- javascript/api.js
+++ javascript/api.js
@@ -58,9 +58,79 @@ fetch('https://osu-api.onrender.com/leaderboard')
         let i = 1;
         for(const a of data.ranking){
             console.log(a.user.username);
+            let b = '';
+            
+            switch(a.user.country.name) {
+                case 'Australia':
+                    b = 'obrázky/flags/Australia.png';
+                    break;
+                case 'Brazil':
+                    b = 'obrázky/flags/Brazil.png';
+                    break;
+                case 'Canada':
+                    b = 'obrázky/flags/Canada.png';
+                    break;
+                case 'Chile':
+                    b = 'obrázky/flags/Chile.png';
+                    break;
+                case 'Germany':
+                    b = 'obrázky/flags/Germany.png';
+                    break;
+                case 'Indonesia':
+                    b = 'obrázky/flags/Indonesia.png';
+                    break;
+                case 'Japan':
+                    b = 'obrázky/flags/Japan.png';
+                    break;
+                case 'Kazakhstan':
+                    b = 'obrázky/flags/Kazakhstan.png';
+                    break;
+                case 'Malaysia':
+                    b = 'obrázky/flags/Malaysia.png';
+                    break;
+                case 'Netherlands':
+                    b = 'obrázky/flags/Netherlands.png';
+                    break;
+                case 'New Zealand':
+                    b = 'obrázky/flags/NewZealand.png';
+                    break;
+                case 'Norway':
+                    b = 'obrázky/flags/Norway.png';
+                    break;
+                case 'Peru':
+                    b = 'obrázky/flags/Peru.png';
+                    break;
+                case 'Philippines':
+                    b = 'obrázky/flags/Philippines.png';
+                    break;
+                case 'Poland':
+                    b = 'obrázky/flags/Poland.png';
+                    break;
+                case 'Qatar':
+                    b = 'obrázky/flags/Qatar.png';
+                    break;
+                case 'Russian Federation':
+                    b = 'obrázky/flags/Russia.png';
+                    break;
+                case 'South Korea':
+                    b = 'obrázky/flags/SouthKorea.png';
+                    break;
+                case 'Spain':
+                    b = 'obrázky/flags/Spain.png';
+                    break;
+                case 'United Kingdom':
+                    b = 'obrázky/flags/UK.png';
+                    break;
+                case 'United States':
+                    b = 'obrázky/flags/US.png';
+                    break;
+                default:
+                    b = 'obrázky/flags/US.png'
+            }
+
             var leaderboardPlayer = createLeaderboardPlayer(
                 i,
-                'obrázky/Flag_of_Australia_(1901–1903).svg.png',
+                b,
                 a.user.username,
                 a.hit_accuracy.toFixed(2) + '%',
                 Math.round(a.pp),
